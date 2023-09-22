415d444bec38a30da21ed9af1bd12d2ddf1267e8
Renan <renan_gian@hotmail.com>
2023-09-22 08:23:42 -0300

    feat: add theme on native-base provider

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
 import { NativeBaseProvider, Box } from "native-base";
+import { theme } from "./src/styles/theme";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "react-native";
+import { SignIn } from "@screens/SignIn";
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Box>Hello world</Box>
+    <NativeBaseProvider theme={theme}>
+      <SafeAreaProvider style={{ flex: 1 }}>
+        <StatusBar
+          translucent
+          barStyle="light-content"
+          backgroundColor="transparent"
+        />
+        <SignIn />
+      </SafeAreaProvider>
     </NativeBaseProvider>
   );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+import { extendTheme } from "native-base";
+
+export const theme = extendTheme({
+  colors: {
+    primary: {
+      10: "#fff",
+      50: "#F4F4F4",
+      100: "#A0A0A0",
+      200: "#000000",
+    },
+    purple: {
+      50: "#F7EFFF",
+      100: "#770FDF",
+    },
+  },
+  sizes: {
+    14: 48,
+    33: 148,
+  },
+});
