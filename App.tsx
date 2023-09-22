0865a702ed3393035f50cd57f0a625cf261b80a8
Renan <renan_gian@hotmail.com>
2023-09-22 15:39:30 -0300

    feat: add fonts

--- App.tsx
+++ App.tsx
@@ -3,17 +3,36 @@ import { theme } from "./src/styles/theme";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "react-native";
 import { SignIn } from "@screens/SignIn";
+import { SignUp } from "@screens/SignUp";
+import { DashBoard } from "@screens/DashBoard";
+import {
+  useFonts,
+  Sora_100Thin,
+  Sora_300Light,
+  Sora_400Regular,
+  Sora_500Medium,
+  Sora_700Bold,
+} from "@expo-google-fonts/sora";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Sora_100Thin,
+    Sora_300Light,
+    Sora_400Regular,
+    Sora_500Medium,
+    Sora_700Bold,
+  });
+
   return (
     <NativeBaseProvider theme={theme}>
       <SafeAreaProvider style={{ flex: 1 }}>
         <StatusBar
           translucent
-          barStyle="light-content"
+          barStyle="dark-content"
           backgroundColor="transparent"
         />
-        <SignIn />
+        {fontsLoaded ? <DashBoard /> : <Loading />}
       </SafeAreaProvider>
     </NativeBaseProvider>
   );
