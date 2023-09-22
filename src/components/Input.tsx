25a0f7c522ce412e4e87432b29f786e89c3e3409
Renan <renan_gian@hotmail.com>
2023-09-22 09:46:47 -0300

    refactor: add label on Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,24 +2,34 @@ import {
   Input as NativeBaseInput,
   IInputProps,
   FormControl,
+  Text,
 } from "native-base";
 
 type Props = IInputProps & {
   errorMessage?: string | null;
+  label?: string;
 };
 
-export function Input({ errorMessage = null, isInvalid, ...rest }: Props) {
+export function Input({
+  errorMessage = null,
+  isInvalid,
+  label,
+  ...rest
+}: Props) {
   const invalid = !!errorMessage || isInvalid;
 
   return (
     <FormControl isInvalid={invalid} mb={4}>
+      <Text fontSize={"11px"} bold color={"primary.100"} mb={2}>
+        {label}
+      </Text>
       <NativeBaseInput
         bg="primary.50"
         h={14}
         px={4}
         borderWidth={0}
-        fontSize="md"
-        color="primary.100"
+        fontSize="sm"
+        color="primary.200"
         fontFamily="body"
         placeholderTextColor="primary.100"
         isInvalid={invalid}
