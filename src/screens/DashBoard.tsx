6acc3738b5898f6051be60dfc3a808de8e29ee49
Renan <renan_gian@hotmail.com>
2023-09-22 15:42:11 -0300

    feat: render components on Dashboard Page

---
+++ src/screens/DashBoard.tsx
@@ -0,0 +1,32 @@
+import { EarnInfo } from "@components/EarnInfo";
+import { Funds } from "@components/Funds/Index";
+import { Header } from "@components/Header";
+import { Divider, ScrollView } from "native-base";
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export function DashBoard() {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+      p={"20px"}
+    >
+      <View
+        style={{
+          flex: 1,
+          paddingTop: insets.top,
+        }}
+      >
+        <Header />
+        <EarnInfo />
+
+        <Divider mt={10} color={"primary.50"} height={0.4} />
+
+        <Funds />
+      </View>
+    </ScrollView>
+  );
+}
