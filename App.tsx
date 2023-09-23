fcbc73daecd8785fb25ec819ff5eef7dcf72d846
Renan <renan_gian@hotmail.com>
2023-09-23 19:48:47 -0300

    feat: add route on APP

--- App.tsx
+++ App.tsx
@@ -2,9 +2,7 @@ import { NativeBaseProvider, Box } from "native-base";
 import { theme } from "./src/styles/theme";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StatusBar } from "react-native";
-import { SignIn } from "@screens/SignIn";
-import { SignUp } from "@screens/SignUp";
-import { DashBoard } from "@screens/DashBoard";
+
 import {
   useFonts,
   Sora_100Thin,
@@ -14,6 +12,10 @@ import {
   Sora_700Bold,
 } from "@expo-google-fonts/sora";
 import { Loading } from "@components/Loading";
+
+import store from "./src/store";
+import { Provider } from "react-redux";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,14 +28,16 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={theme}>
-      <SafeAreaProvider style={{ flex: 1 }}>
-        <StatusBar
-          translucent
-          barStyle="dark-content"
-          backgroundColor="transparent"
-        />
-        {fontsLoaded ? <DashBoard /> : <Loading />}
-      </SafeAreaProvider>
+      <Provider store={store}>
+        <SafeAreaProvider style={{ flex: 1 }}>
+          <StatusBar
+            translucent
+            barStyle="dark-content"
+            backgroundColor="transparent"
+          />
+          {fontsLoaded ? <Routes /> : <Loading />}
+        </SafeAreaProvider>
+      </Provider>
     </NativeBaseProvider>
   );
 }
