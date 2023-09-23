a9f398a6c4af786efce58c7ffb4349e12aa0101a
Renan <renan_gian@hotmail.com>
2023-09-23 19:46:17 -0300

    feat: create terms page

---
+++ src/screens/Terms.tsx
@@ -0,0 +1,25 @@
+import { ScrollView, Text } from "native-base";
+
+export function Terms() {
+  return (
+    <ScrollView style={{ padding: 20 }}>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor
+        velit a pretium malesuada. Suspendisse quis dui id risus iaculis
+        gravida. Cras consectetur, quam quis pulvinar hendrerit, urna ex pretium
+        risus, vitae tempor justo urna eu orci. Vivamus viverra tellus nisi, id
+        ultricies mi elementum sed. Morbi ac venenatis arcu, non volutpat
+        turpis. In consectetur malesuada lacus non dapibus. Donec suscipit, est
+        sit amet congue accumsan, lacus magna hendrerit mi, non finibus lorem
+        ante ac mi. Nunc eu scelerisque tellus, at blandit mi. Cras ullamcorper
+        facilisis risus non molestie. Aenean cursus libero risus. Proin ornare
+        tortor ac lacinia sodales. Morbi congue egestas neque. Suspendisse risus
+        mi, condimentum et fermentum ut, blandit sed lacus. Sed eu enim sem.
+        Proin venenatis non metus et consequat. Curabitur non hendrerit libero.
+        Donec in ligula eros. Fusce pharetra nisl purus. Donec vitae magna
+        pellentesque tellus accumsan porta nec quis felis. Donec turpis turpis,
+        dictum ut ligula a, vestibulum feugiat est.
+      </Text>
+    </ScrollView>
+  );
+}
