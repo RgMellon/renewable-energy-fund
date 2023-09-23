cae071d900d3620c047114d11ca47206bb77e8dc
Renan <renan_gian@hotmail.com>
2023-09-23 16:53:47 -0300

    chore: mocking de info data

---
+++ src/@mocks/infoMock.ts
@@ -0,0 +1,14 @@
+export const infoMocks = [
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_kFBDRE3t_MOhx7BCpgxAAJMps6E5yz8hC6HvTnR_OG-UgpGUvTSAc0FYhcTXm5h0hKE&usqp=CAU",
+    title: "Aspira Dac",
+    description:
+      "Aspira is building a modular, direct air capture system with the energy supply integrated into the modules.",
+  },
+  {
+    img: "https://www.wipo.int/export/sites/www/wipo_magazine/images/en/2020/2020_01_art_1_4_845.jpg",
+    title: "ClimeWorks",
+    description:
+      "uses renewable geothermal energy and waste heat to capture CO directly from the air",
+  },
+];
