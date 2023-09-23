c94c65c5ab0a90c31568ac9d9455f29cc7fe5bcc
Renan <renan_gian@hotmail.com>
2023-09-23 19:46:34 -0300

    chore: add type on navigation

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,10 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      SignUp: undefined;
+      SignIn: undefined;
+      Terms: undefined;
+      FundDetail: undefined;
+    }
+  }
+}
